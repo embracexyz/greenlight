17630286d94b19e5d1fafc18c9320cc3b0297f59
embracexyz <asynchack@gmail.com>
2025-07-15 09:12:15 +0800

    parse json request

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -29,5 +31,18 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 }
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from createMovieHandler"))
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	fmt.Printf("%+v\n", input)
 }
