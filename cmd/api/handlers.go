892b6313a94508e1ea3dfa6ff13f2e9bec8f93d4
embracexyz <asynchack@gmail.com>
2025-07-19 15:16:19 +0800

    add partial update

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -159,3 +159,73 @@ func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Reques
 	}
 
 }
+
+func (app *application) partialUpdateMovieHandler(w http.ResponseWriter, r *http.Request) {
+	// 1. 解析url id是否合法，否则return badRequest
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	// 2. 查询id是否真实存在，否则return nodFound
+	movie, err := app.models.MovieModel.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	// 3. 把更新的值，覆盖从数据库查询的字段（其他字段保留）
+	// ! 把值类型改为其指针，这样json解析时，没传值的就会保持为nil，根据是否为nil可判断客户端是否传值，只针对传值的字段进行覆盖更新，实现partialUpdate的效果
+	//		如果是"key": null，默认json解析器也会忽略该值，认为没传；另注意"key": ""，是传值了，值是空字符串
+	var input struct {
+		Title   *string       `json:"title"`
+		Year    *int32        `json:"year"`
+		Runtime *data.Runtime `json:"runtime"`
+		Genres  []string      `json:"genres"`
+	}
+
+	err = app.readJson(w, r, &input)
+	if err != nil {
+		app.badRequestErrorReponse(w, r, err)
+		return
+	}
+
+	if input.Title != nil {
+		movie.Title = *input.Title
+	}
+	if input.Year != nil {
+		movie.Year = *input.Year
+	}
+	if input.Runtime != nil {
+		movie.Runtime = *input.Runtime
+	}
+	if input.Genres != nil {
+		movie.Genres = input.Genres
+	}
+
+	// 4. validatror严重，否则return， baserequest
+	v := validator.New()
+	if data.ValidateMove(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.FieldErrors)
+		return
+	}
+
+	// 5. 更新，err则判断err类型，返回对应响应，happy path则write更新后的json
+	err = app.models.MovieModel.Update(movie)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJson(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
+}
